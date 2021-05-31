3fe1d14250e537d95c93c72dbdcd134283169c48
piton322 <baobab000@mail.ru>
2021-05-31 14:36:21 +0300

    hw5 with comments

--- src/network/st_nonblocking/Connection.h
+++ src/network/st_nonblocking/Connection.h
@@ -1,26 +1,37 @@
 #ifndef AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
 #define AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
 
+#include "afina/Storage.h"
+#include "afina/logging/Service.h"
+#include "afina/execute/Command.h"
+#include "spdlog/logger.h"
 #include <cstring>
-
+#include <deque>
 #include <sys/epoll.h>
+#include "protocol/Parser.h"
+namespace Afina 
+{
+namespace Network 
+{
+namespace STnonblock 
+{
 
-namespace Afina {
-namespace Network {
-namespace STnonblock {
-
-class Connection {
+class Connection 
+{
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps) 
+        : _socket(s), _alive(true), pStorage(ps)
+    {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
     }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return _alive; } // периодически вызывается epoll'ом с целью понять, не хотим ли бы убить этот connetion
 
-    void Start();
+    void Start(); // вызывается сразу после создания потоком, который делаем epoll, ждем ввода и устанавливаем интерес на чтение
 
 protected:
+    // методы вызываются в зависимости от того, что можно сделать с сокетом
     void OnError();
     void OnClose();
     void DoRead();
@@ -29,8 +40,21 @@ protected:
 private:
     friend class ServerImpl;
 
-    int _socket;
-    struct epoll_event _event;
+    bool _alive;
+    int _socket; // сокет, который обрабатывается
+    struct epoll_event _event; // евенты, с которыми connection хочет быть добавлен в epoll дескриптор
+
+    std::size_t _w_offset; // отступ при чтени
+    std::size_t _r_offset; // отступ при записи
+    std::deque<std::string> _output_queue; // очередь, куда будем записывать результаты
+
+    std::shared_ptr<Afina::Storage> pStorage;
+    // для чтения (взято из предыдущих дз)
+    std::size_t arg_remains;
+    Protocol::Parser _parser;
+    std::string argument_for_command;
+    std::unique_ptr<Execute::Command> command_to_execute;
+    char client_buffer[4096] = "";
 };
 
 } // namespace STnonblock
